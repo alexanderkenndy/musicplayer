db3f58d5094d1f81ed816b5915abce3afefd4720
alexanderkenndy <alexander.kenndy@gmail.com>
2014-06-01 01:23:44 +0800

    add ejs template

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 var http = require('http');
 var express = require('express');
 var app = express();
+var server = http.createServer(app);
+app.set('view engine',"ejs");
+app.set('views',__dirname + '/views/');
+app.use(express.logger());
+app.use(express.methodOverride());
+app.use(express.cookieParser('alexander'));
+app.use(express.cookieSession());
+app.use(express.bodyParser());
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/assets'));
+app.use(express.static(__dirname + '/thirdparty'));
 app.get('/',function(req,res){
-	res.send('hello');
+	res.render('index',{});
 });
 app.listen(3000,function(err){
 	console.log('Server running on port 3000');
